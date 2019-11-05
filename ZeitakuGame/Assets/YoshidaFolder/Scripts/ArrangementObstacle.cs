77907b41351b7d3555f1de00e37d522ad0341cf7
Yoshdia <yoshiatsu02@gmail.com>
2019-11-05 18:59:26 +0900

    障害物のランダム生成に成功

--- ZeitakuGame/Assets/YoshidaFolder/Scripts/ArrangementObstacle.cs
+++ ZeitakuGame/Assets/YoshidaFolder/Scripts/ArrangementObstacle.cs
@@ -1,69 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+
 
 public class ArrangementObstacle : MonoBehaviour
 {
-    [SerializeField]
-    List<Transform> flags;
-    int flagsCount = 0;
-    int flagsCountMax = 0;
+    Transform movement;
+    //描画する座標のフラグを所持しているクラス
     [SerializeField]
     PositionFlags positionFlags;
-
+    //PositionFlagsクラスから受け取る座標のリスト
+    [SerializeField]
+    List<Transform> flags;
+    //リストのアクセスに使用する要素数
+    int flagsCount = 0;
+    //再生速度 
     float time = 0;
+
+    //ステージの端になるポール
     [SerializeField]
     GameObject poll;
 
-    Transform now;
-    Transform next;
+    //poll生成のインターバル
+    int possInstantiateCount = 0;
 
-    int instantiateCount = 0;
+    //障害物生成のインターバル
+    int obstacleInstantiateCount = 0;
+
+    //生成する障害物のセットとそれぞれの生成確率
+    [System.Serializable]
+    struct Obstacles
+    {
+        //障害物を格納する障害物配列　
+        public GameObject obstacleArray;
+        public float perf;
+    }
+    public GameObject ob;
+
+    [SerializeField]
+    Obstacles[] obstacles;
+
+    float obstaclesRandSum = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         positionFlags = GameObject.Find("StageFlagg").GetComponent<PositionFlags>();
         flags = new List<Transform>();
         flags = positionFlags.GetList();
-        flagsCountMax = flags.Count;
         flagsCount = 0;
-        now = flags[0];
-        next = flags[1];
         time = 0;
-        instantiateCount = 0;
+        possInstantiateCount = 0;
+        movement = transform;
+        for(int num=0;num<obstacles.Length;num++)
+        {
+            obstaclesRandSum += obstacles[num].perf;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (flagsCount + 3 >= flags.Count)
+        {
+            return;
+        }
+        BezierCurveMove();
+        SidePollInstantiate();
+        ObstacleInstantiate();
+        transform.position = movement.position;
+    }
+
+    //計算された軌道に反り移動する
+    void BezierCurveMove()
+    {
         time += 0.005f;
-        //transform.position = Vector3.Lerp(now.position, next.position, time);
-        //transform.position = Vector3.Slerp(now.position, next.position, time);
-        transform.position=GetPoint(flags[flagsCount].position, flags[flagsCount+1].position, flags[flagsCount+2].position, flags[flagsCount+3].position, time);
+        movement.position = GetPoint(flags[flagsCount].position, flags[flagsCount + 1].position, flags[flagsCount + 2].position, flags[flagsCount + 3].position, time);
+        //再生が終了したら次のフラグへ
         if (time >= 1)
         {
             flagsCount += 3;
-            now = flags[flagsCount];
-            next = flags[flagsCount + 1];
             time = 0;
         }
-        if (flagsCount >= flagsCountMax)
-        {
-            Debug.Log("ed");
-        }
 
-        if (instantiateCount > 10)
+    }
+
+    void SidePollInstantiate()
+    {
+        //ポールの生成
+        if (possInstantiateCount > 10)
         {
             Instantiate(poll, new Vector3(transform.position.x + 10, transform.position.y, transform.position.z), new Quaternion());
             Instantiate(poll, new Vector3(transform.position.x - 10, transform.position.y, transform.position.z), new Quaternion());
-            instantiateCount = 0;
+            possInstantiateCount = 0;
         }
         else
         {
-            instantiateCount++;
+            possInstantiateCount++;
         }
     }
 
+    //ベジェ曲線の計算
     Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
         var a = Vector3.Lerp(p0, p1, t); // 緑色の点1
@@ -74,5 +112,41 @@ public class ArrangementObstacle : MonoBehaviour
         var e = Vector3.Lerp(b, c, t);   // 青色の点2
 
         return Vector3.Lerp(d, e, t);    // 黒色の点
+    }
+
+    void ObstacleInstantiate()
+    {
+        if (obstacleInstantiateCount > 50)
+        {
+            Vector3 pos = transform.position;
+            //Quaternion qua = transform.rotation - movement.rotation;
+            GameObject a = GetRandamObstacle();
+            a = Instantiate(a, transform.position, new Quaternion());
+            obstacleInstantiateCount = 0;
+        }
+        else
+        {
+            obstacleInstantiateCount++;
+        }
+    }
+
+    GameObject GetRandamObstacle()
+    {
+        GameObject obj;
+        float rand = Random.Range(0,obstaclesRandSum);
+        for (int num = 0; num < obstacles.Length; num++)
+        {
+            float bef = 0;
+            for(int b=0;b<num;b++)
+            {
+                bef += obstacles[b].perf;
+            }
+
+            if(rand<=obstacles[num].perf+bef)
+            {
+                return obstacles[num].obstacleArray;
+            }
+        }
+            return obstacles[0].obstacleArray;
     }
 }
