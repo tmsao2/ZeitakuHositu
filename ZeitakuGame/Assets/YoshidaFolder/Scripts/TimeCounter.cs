db6cb4bc9032ce02c6b889ff72abb885474c2f76
Yoshdia <yoshiatsu02@gmail.com>
2019-11-06 20:53:55 +0900

    ゴールモデル追加、制限時間を追加

---
+++ ZeitakuGame/Assets/YoshidaFolder/Scripts/TimeCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    public float timeRimit = 0;
+    [SerializeField]
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "" + timeRimit;
+
+        if (timeRimit <= 0)
+        {
+            Debug.Log("timeRimit");
+        }
+        else
+        {
+            timeRimit -= Time.deltaTime;
+
+        }
+    }
+}
